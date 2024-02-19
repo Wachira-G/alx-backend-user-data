41a587aceb4418b43455150982c2abb63f45cdf6
Gabriel Wachira <gabbyk.com@gmail.com>
2024-02-19 15:04:24 +0300

    extract user credentials: decoding

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -47,3 +47,17 @@ class BasicAuth(Auth):
             return True
         except Exception:
             return False
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str
+    ) -> (str, str):
+        """Returns the user email and password from the Base64 decoded value"""
+        if (
+                decoded_base64_authorization_header is None
+                or not isinstance(decoded_base64_authorization_header, str)
+                or ':' not in decoded_base64_authorization_header
+        ):
+            return None, None
+        splitted_string_list = decoded_base64_authorization_header.split(':')
+        return splitted_string_list[0], splitted_string_list[1]
