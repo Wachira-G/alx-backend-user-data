37ef89d9a73b74add72fffea6088840fe0fcb2db
Gabriel Wachira <gabbyk.com@gmail.com>
2024-02-19 10:52:47 +0300

    define which routes need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,13 +12,22 @@ class Auth:
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Returns False."""
+        """Returns True if path is not in the list of strings excluded_paths"""
+        if path and not path.endswith("/"):
+            path = path + "/"
+        if (
+            excluded_paths is None
+            or path is None
+            or path not in excluded_paths
+            or len(excluded_paths) == 0
+        ):
+            return True
         return False
 
     def authorization_header(self, request=None) -> str:
         """Returns None."""
         return None
 
-    def current_user(self, request=None) -> TypeVar('User'):
+    def current_user(self, request=None) -> TypeVar("User"):
         """Returns None."""
         return None
