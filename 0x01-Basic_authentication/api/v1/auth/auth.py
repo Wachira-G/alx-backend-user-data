08b9aa96f9b1b98903cdf34afa43756f6a5d2952
Gabriel Wachira <gabbyk.com@gmail.com>
2024-02-19 12:29:35 +0300

    add request validation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -26,7 +26,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """Returns None."""
-        return None
+        if request is None or not request.headers.get("Authorization"):
+            return None
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar("User"):
         """Returns None."""
