7d197dc15bd83b547fe44e21130cb669d8619446
Gabriel Wachira <gabbyk.com@gmail.com>
2024-02-21 16:28:04 +0300

    add create a session task

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 
 """Session auth module"""
 
+import uuid
 from api.v1.auth.auth import Auth
 
 
@@ -11,4 +12,14 @@ class SessionAuth(Auth):
     validate if everything inherits correctly without overloading
     validate the 'switch' by using environ variables."""
 
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user_id."""
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return user_id
