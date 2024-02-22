24065bad475712cfd8ec5745cabc72067c7f3637
Gabriel Wachira <gabbyk.com@gmail.com>
2024-02-22 16:12:31 +0300

    sessions in database

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,1 +1,38 @@
 #!/usr/bin/env python3
+
+"""Module for session db authentication."""
+
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from models.user_session import UserSession
+
+
+class SessionDBAuth(SessionExpAuth):
+    """sesssion db auth class."""
+
+    def create_session(self, user_id=None):
+        "Creates, stores new instance of UserSession and returns session id."""
+        session_id = super().create_session(user_id)
+        if not session_id:
+            return None
+        user_session = UserSession(
+                {"user_id": user_id, "session_id": session_id}
+        )
+        if not user_session:
+            return None
+        return session_id
+
+    def destroy_session(self, request=None):
+        """Destroy the UserSession based on Session ID from request cookie."""
+        cookie = session_cookie(request)
+        if cookie:
+            user_id = self.user_id_for_session_id(cookie)
+            session_id = cookie
+            user_session = UserSession.get(sessiono_id)
+            if not user_session:
+                return False
+            del user_session
+            cookie_deleted = super().destroy_session(request)
+            if not cookie_deleted:
+                return False
+            return True
+        return False
--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+
+"""Module for user session."""
+
+from models.base import Base
+
+
+class UserSession(Base):
+    """User session class."""
+    def __init__(self, *args: list, **kwargs: dict):
+        """Initialise user instance."""
+        super().__init__(*args, **kwargs)
+        self.user_id: string = kwargs.get("user_id")
+        self.session_id: string = kwargs.get("session_id")
