10fe07ee9ac84cfd4361e6868f3de9b955ab7fe3
Gabriel Wachira <gabbyk.com@gmail.com>
2024-02-22 23:13:21 +0300

    add register a user task

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python3
 
 """Flask app module."""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def home() -> str:
     """home route."""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """register a user."""
+    if not request.form:
+        abort(401)
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not email or not password:
+        abort(401)
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError as err:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": email, "message": "user created"}), 200
 
 
 if __name__ == "__main__":
