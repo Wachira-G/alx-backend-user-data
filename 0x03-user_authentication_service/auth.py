13d4311e681ab552f3a489c841ec806fff72e930
Gabriel Wachira <gabbyk.com@gmail.com>
2024-02-23 07:57:06 +0300

    fix typo

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -61,7 +61,7 @@ class Auth:
             user = self._db.find_user_by(email=email)
             if user:
                 session_uuid = _generate_uuid()
-                self._db.update_user(user_id, session_id=session_uuid)
+                self._db.update_user(user.id, session_id=session_uuid)
                 return session_uuid
         except NoResultFound:
             return None
