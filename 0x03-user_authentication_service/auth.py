b4c47478d2a85a7ad3142fd4d205631d787e1df5
Gabriel Wachira <gabbyk.com@gmail.com>
2024-02-25 08:12:33 +0300

    add update password task

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -101,3 +101,14 @@ class Auth:
             return reset_token
         except NoResultFound as err:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str):
+        """updates a users password after reset."""
+        if not token or not password:
+            raise ValueError
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_password)
+        except NoResultFound as err:
+            raise ValueError
